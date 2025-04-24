7d121cbcc99f4b01ccf72cd7e326ea4c591bc7f7
ghks8525 <ghks8525@gamil.com>
2025-04-24 18:27:56 +0900

    refactor(provider) : folderRepoProvider와 httpClientProvider 성격이 달라 repo_provider.dart 생성 후 분리

--- lib/provider/client_provider.dart
+++ lib/provider/client_provider.dart
@@ -1,14 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:krap/provider/dio_provider.dart';
-import 'package:krap/repository/http/HttpClient.dart';
-import 'package:krap/repository/http/FolderRepository.dart';
+import 'package:krap/repository/http/http_client.dart';
 
-final HttpClientProvider = Provider<HttpClient>((ref) {
+final httpClientProvider = Provider<HttpClient>((ref) {
   final dio = ref.watch(dioProvider);
   return HttpClient(dio);
 });
 
-final userRepoProvider = Provider<FolderRepository>((ref) {
-  final client = ref.watch(HttpClientProvider);
-  return FolderRepository(client);
-});
