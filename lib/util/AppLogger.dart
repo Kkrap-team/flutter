3439bbc2e974e499b618387831617221214c6785
connot <connor@hugapp.co.kr>
2025-04-24 16:09:37 +0900

    feat(AppLogger): Logger 라이브러리 wrapper 로그 출력시 기본적으로 공통 태그 사용

---
+++ lib/util/AppLogger.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:logger/logger.dart';
+
+class AppLogger {
+  static final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      methodCount: 1,
+      colors: true,
+      printEmojis: true,
+    ),
+  );
+  static void v(message, {String tag = 'Krap'}) {
+    if (!kReleaseMode) _logger.v(_format(message, tag));
+  }
+
+  static void d(message, {String tag = 'Krap'}) {
+    if (!kReleaseMode) _logger.d(_format(message, tag));
+  }
+
+  static void i(message, {String? tag = 'Krap'}) {
+    if (!kReleaseMode) _logger.i(_format(message, tag));
+  }
+
+  static void w(message, {String? tag = 'Krap'}) {
+    if (!kReleaseMode) _logger.w(_format(message, tag));
+  }
+
+  static void e(message, {String? tag = 'Krap'}) {
+    if (!kReleaseMode) _logger.e(_format(message, tag));
+  }
+
+  static String _format(message, String? tag) {
+    return tag != null ? '[$tag] $message' : message.toString();
+  }
+}
