000daac84b9efcf1ee7d0b19a8ef0c44b887fef3
ghks8525 <ghks8525@gamil.com>
2025-04-24 20:05:44 +0900

    refactor(baseUrl) : github에 baseUrl 안올라가도록 수정 로컬의 config/config.dart 사용으로 변경

--- lib/provider/dio_provider.dart
+++ lib/provider/dio_provider.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:krap/config/config.dart';
 import 'package:krap/repository/http/custom_log_interceptor.dart';
 
 final dioProvider = Provider<Dio>((ref) {
   Dio dio = Dio(BaseOptions(
+    baseUrl: Env.baseUrl,
     connectTimeout: const Duration(seconds: 5),
     receiveTimeout: const Duration(seconds: 5),
   ));
