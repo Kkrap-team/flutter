5081130e52d2379e10cab383b44f16c5e206f659
connot <connor@hugapp.co.kr>
2025-04-24 16:25:43 +0900

    feat(CustomLogInterceptor): dio 로그 인터셉터 커스터마이징 Request, Error, Response 시 요청, 응답 데이터 로깅

---
+++ lib/repository/http/CustomLogInterceptor.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:krap/util/AppLogger.dart';
+import 'package:krap/util/Util.dart';
+
+class CustomLogInterceptor extends LogInterceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (kDebugMode) {
+      AppLogger.i('[REQUEST] => ${options.method} ${options.uri}');
+      AppLogger.d('Headers: ${options.headers}');
+      if (options.data != null) {
+        AppLogger.d('Body:\n${Util.convertJsonToString(options.data)}');
+      }
+      super.onRequest(options, handler);
+    }
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    if (kDebugMode) {
+      AppLogger.e('[ERROR] => ${err.message}');
+      if (err.response != null) {
+        AppLogger.w('Response:\n${Util.convertJsonToString(err.response?.data)}');
+      }
+      super.onError(err, handler);
+    }
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    if (kDebugMode) {
+      AppLogger.i(
+        '[RESPONSE] => ${response.statusCode} ${response.requestOptions.uri}',
+      );
+      AppLogger.d('Data:\n${Util.convertJsonToString(response.data)}');
+      super.onResponse(response, handler);
+    }
+  }
+
+}
