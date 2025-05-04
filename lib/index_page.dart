b833ecdc80092d89295e9c238541779c2c94feac
ghks8525 <ghks8525@gmail.com>
2025-05-04 18:58:59 +0900

    feat(index): 화면 테스트용 page추가

---
+++ lib/index_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:krap/core/navigation/app_router.dart';
+
+class IndexPage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: SquareTextButton("tutorial", () {
+                    AppRouter.goTutorial(context);
+                  }),
+                ),
+                Expanded(
+                  child: SquareTextButton("login", () {
+                    AppRouter.goLogin(context);
+                  }),
+                ),
+                Expanded(child: SquareTextButton("", () {})),
+                Expanded(child: SquareTextButton("", () {})),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(child: SquareTextButton("", () {})),
+                Expanded(child: SquareTextButton("", () {})),
+                Expanded(child: SquareTextButton("", () {})),
+                Expanded(child: SquareTextButton("", () {})),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget SquareTextButton(String data, VoidCallback onPressed) {
+    return TextButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        side: WidgetStateProperty.all(
+          const BorderSide(color: Colors.grey, width: 1), // 테두리 색상과 두께
+        ),
+        shape: WidgetStateProperty.all(
+          const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        ),
+        padding: WidgetStateProperty.all(
+          const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        ),
+      ),
+      child: Text(data),
+    );
+  }
+}
