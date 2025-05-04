9eb646fa9021cb3d0dec848c38d028e9daea5f20
ghks8525 <ghks8525@gmail.com>
2025-05-04 18:53:22 +0900

    feat(style): ui에서 사용하는 공통 color 및 textStyle 정의

---
+++ lib/common/styles/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const black = Color(0xff000000);
+  static const primary = Color(0xff5AB7FC);
+  static const grey = Color(0xffb3b3b3);
+  static const transparent = Color(0x00000000);
+  static const white = Color(0xffffffff);
+}
